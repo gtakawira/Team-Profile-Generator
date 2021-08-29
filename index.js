3cecb77d50d20bca28d13aa75312ddfe15cf7152
blacxcellence <tgtakawira@gmail.com>
2021-08-29 15:49:57 -0500

    cards are generating

---
+++ index.js
@@ -0,0 +1,133 @@
+const inquirer = require("inquirer")
+const fs=require('fs')
+const chalk = require('chalk');
+const job= require('./lib/classes');
+const htmlCode = require("./src/renderHTML");
+
+
+const managerqs=[{
+    type: 'input',
+    name: 'name',
+    message: `What is the Manager's name?`
+},
+{
+    type: 'input',
+    name: 'id',
+    message: 'What is the employees id number?'
+},
+{
+    type: 'input',
+    name: 'email',
+    message: 'What is the employees email address?'
+},
+{
+    type: 'input',
+    name: 'office',
+    message: 'What is the office number?',
+    
+},
+{   type: 'list',
+        name: 'save',
+        message: `Do you want to save this employee?`,
+        choices: ['Save', 'Cancel'],
+                
+    },   
+
+]
+const questions = [
+    {
+        type: 'list',
+        name: 'title',
+        message: `Select an employee type to add.`,
+        choices: ['Engineer', 'Intern', 'Generate'],
+        
+        
+        
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the employees name?',
+        when: (questions) => questions.title !== 'Generate'
+
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the employees id number?',
+        when: (questions) => questions.title !== 'Generate'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the employees email address?',
+        when: (questions) => questions.title !== 'Generate'
+    },
+    {
+        type: 'input',
+        name: 'office',
+        message: 'What is the office number?',
+        when: (questions) => questions.title === 'Manager'
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is the employees GitHub username?',
+        when: (questions) => questions.title === 'Engineer'
+    },
+
+    {
+        type: 'input',
+        name: 'school',
+        message: `What is the intern's school name?`,
+        when: (questions) =>  questions.title === 'Intern'
+    },
+    {   type: 'list',
+        name: 'save',
+        message: `Do you want to save this employee?`,
+        choices: ['Save', 'Cancel'],
+        when: (questions) => questions.title !== 'Generate'
+
+        
+    },   
+    
+]
+
+
+let team = []
+function init() {
+    console.log(chalk.blue(`
+    ********************************************************************
+    Let generate your staff profile? Answer the following Questions
+    *******************************************************************`));
+    inquirer.prompt(managerqs).then((data) => {
+        
+        (data.save=='Save') ? team.push(new job.Manager(data.name, data.id, data.email, data.office)) & menu() : console.log('Cancelled') ;
+        
+
+       
+    });
+}
+    //const added=job.generateObject() || ''
+
+       
+        function writeToFile(file, data) { 
+    
+            fs.writeFile(file, data, (err) => {
+            err ? console.log(err) : console.log("Success!");
+        })
+        menu()
+        }
+
+        
+init();
+
+    function menu(){ inquirer.prompt(questions).then((data) => {
+        
+        
+        data.save=='Save' & data.title === 'Engineer' ? team.push(new job.Engineer(data.name, data.id, data.email,  data.username)) & menu():
+        data.title === 'Intern' & data.save==='Save' ? team.push(new job.Intern(data.name, data.id, data.email,  data.school)) & menu():
+        data.title === 'Generate' ?  writeToFile('rendered.html',htmlCode(team)):console.log(' Cancelled') })}
+            
+        
+    
---
+++ src/renderHTML.js
@@ -0,0 +1,99 @@
+const { createReadStream } = require('fs')
+const generateObject = require('../lib/classes')
+
+// let cards=append(Card)
+const htmlCode = data => {
+  let cards=[];
+  for (let i = 0; i < data.length; i++) {
+    cards.push(cardgen(data[i]));
+  
+    ;
+ 
+ 
+     }
+    
+    let fincards=cards.join('')
+
+  
+    return`<!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>The Team</title>
+      <!-- css Reset CDN -->
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css" />
+      <!-- googleFonts -->
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet">
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=PT+Sans&display=swap" rel="stylesheet">
+      <!-- Framework CDN -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
+      <!-- FontAwesome -->
+      <script src="https://kit.fontawesome.com/6df6b95a29.js" crossorigin="anonymous"></script>
+    
+      <!-- Style sheet -->
+      <link rel="stylesheet" href="./style.css" />
+    </head>
+    
+    <body>
+      <nav>
+        <div class="nav-wrapper green lighten-5">
+          <a href="../index.html" class="brand-logo center teal-text text-darken-4">The Team</a>
+         
+        </div>
+      </nav>
+    
+      <!--three same size container-->
+      <div class="container center-align">
+    
+        <div class="row" id="container">
+         
+        ${fincards}
+        
+        </div>
+        <a class="waves-effect waves-light btn-large" id="reset">CLEAR</a>
+      </div>
+    
+      <!--jQuery -->
+      <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+      <!-- jQueryUI -->
+      <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
+        integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
+      <!-- materialize -->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    
+      <!-- our Script -->
+      <script src="./script.js"></script>
+    </body>
+    
+    </html>`
+    
+}
+function cardgen(data) {     
+      
+
+  console.log('render6' ,data)
+  return`<div class="col s12 m12 l4 clickable actCards">
+  <div class="card teal darken-1">
+  <div class="card-content white-text">
+  <span class="card-title">${data.getRole()}</span>
+  <ul id="list" > 
+  <li>${data.getName()}</li>
+  <li> ${data.getId()}</li>
+  <li>${data.getEmail()}</li>
+  </ul> 
+  </div> 
+  </div> 
+  </div>`};
+
+
+
+
+module.exports= htmlCode
